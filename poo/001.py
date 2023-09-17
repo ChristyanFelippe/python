33c3dd15e38afb949ab852d4b1042e0b5c0d4215
ch052877 <christyanmend@gmail.com>
2023-09-17 05:55:08 -0300

    adicionando metodo com  looping for

--- poo/001.py
+++ poo/001.py
@@ -5,14 +5,18 @@ class Televisao():
         self.marca = marca
         self.n_canais = n_canais
         self.preco = preco
-        
-    def aumentar_canais(self, quantidade):
-        self.n_canais += quantidade
+
+    def aumentar_canais(self):
+        self.n_canais += 1
 
 
 tv1 = Televisao('samsung', 2011, 1000, 10)
+
+print(tv1.n_canais)
+tv1.aumentar_canais()
 print(tv1.n_canais)
 
-tv1.aumentar_canais(30)
+for i in range(5):
+    tv1.aumentar_canais()
 
 print(tv1.n_canais)
