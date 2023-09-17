a80eff29461a2847da7011f8c601240bed653297
ch052877 <christyanmend@gmail.com>
2023-09-17 05:10:29 -0300

    criando metodos

--- poo/001.py
+++ poo/001.py
@@ -1,2 +1,18 @@
-class calc():
-    pass
+class Televisao():
+    def __init__(self, marca, ano, preco, n_canais):
+        print(f"O objeto  {self} foi construido")
+        self.ano = ano
+        self.marca = marca
+        self.n_canais = n_canais
+        self.preco = preco
+        
+    def aumentar_canais(self, quantidade):
+        self.n_canais += quantidade
+
+
+tv1 = Televisao('samsung', 2011, 1000, 10)
+print(tv1.n_canais)
+
+tv1.aumentar_canais(30)
+
+print(tv1.n_canais)
